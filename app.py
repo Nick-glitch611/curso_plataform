3ca8187bfb7d05ecde323f3f3715818c1aec9d7b
Nicholas Matheus de Oliveira Bezerra <nicholas.m@escolar.ifrn.edu>
2026-09-01 10:00:54 -0300

    feat: add user registration and login

--- app.py
+++ app.py
@@ -1,17 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+from werkzeug.security import generate_password_hash
+
+conexao = sqlite3.connect("curso_plataform/database.db")
+db = conexao.cursor()
+
+db.execute("""
+    CREATE TABLE IF NOT EXISTS dados (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        usuario TEXT NOT NULL,
+        email TEXT NOT NULL,
+        senha TEXT NOT NULL
+    )
+""")
+
+conexao.commit()
+conexao.close()
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+
+        usuario = request.form["usuario"]
+        senha = generate_password_hash(request.form["senha"])
+
     return render_template("auth/login.html")
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
+    if request.method == "POST":
+
+        usuario = request.form["usuario"]
+        email = request.form["email"]
+        senha1 = generate_password_hash(request.form["senha1"])
+        senha2 = generate_password_hash(request.form["senha2"])
+
+        if senha1 == senha2:
+            db.execute("""
+                INSERT INTO dados (usuario, senha, email)
+                VALUES (?, ?, ?)
+            """, (usuario, email, senha1))
+
     return render_template("auth/cadastro.html")
 
 if __name__ == "__main__":
